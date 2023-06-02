ebed902b9629bfb1b4af200cc7f349ad780ab2d3
Robert Nickson <106345947+RN255@users.noreply.github.com>
2023-06-02 12:04:13 +0100

    js file tidy

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // variables
 var navHamburger = document.getElementsByClassName("navHamburger")[0];
 var navLinksContainer = document.getElementsByClassName("navLinksContainer")[0];
-var header = document.getElementsByClassName("header")[0];
-var navLinksList = document.getElementsByClassName("navLinksList")[0];
 var upperBurgerBar = document.getElementsByClassName("burgerBar")[0];
 var lowerBurgerBar = document.getElementsByClassName("burgerBar")[2];
 var middleBurgerBar = document.getElementsByClassName("burgerBar")[1];
@@ -10,30 +8,29 @@ var middleBurgerBar = document.getElementsByClassName("burgerBar")[1];
 // nav open/closed variable
 var navOpen = false;
 
-// functions
+// open and close nav links
 navHamburger.addEventListener("click", function () {
   if (navOpen == false) {
+    // show nav links
     navLinksContainer.classList.add("navLinksContainerVisible");
-    header.classList.add("fullScreenHeader");
+    // animate burger icon
     upperBurgerBar.classList.add("rotateDown");
     lowerBurgerBar.classList.add("rotateUp");
     middleBurgerBar.classList.add("hideBurgerBar");
+    // prevent scrolling
     document.body.style.overflow = "hidden";
-    // navLinksList.classList.add("navLinksListBlock");
-
+    // change nav open/close variable
     navOpen = true;
   } else {
+    // hide nav links
     navLinksContainer.classList.remove("navLinksContainerVisible");
-    header.classList.remove("fullScreenHeader");
+    // animate burger icon
     upperBurgerBar.classList.remove("rotateDown");
     lowerBurgerBar.classList.remove("rotateUp");
     middleBurgerBar.classList.remove("hideBurgerBar");
+    // allow scrolling
     document.body.style.overflow = "visible";
-
-    // navLinksList.classList.remove("navLinksListBlock");
-
+    // change nav open/close variable
     navOpen = false;
   }
-
-  console.log("clicked");
 });
